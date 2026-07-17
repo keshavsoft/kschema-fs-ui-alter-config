0e72f6d854efa25592036017668aecd80c0bd312
keshavsoft <nknnkumar@yahoo.com>
2026-07-17 12:10:31 +0530

    to test

--- test/v2/get/test.js
+++
@@ -1,7 +0,0 @@
-import path from "path";
-import load from "../../../index.js";
-
-load({
-    toPath: process.cwd(),
-    showLog: true
-});
---
+++ test/v2/test.js
@@ -0,0 +1,7 @@
+import path from "path";
+import load from "../../index.js";
+
+load({
+    toPath: path.join(process.cwd(), "doctors"),
+    configPath: path.join(process.cwd(), "Config", "Schemas", "doctors.json")
+});
