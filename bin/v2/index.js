d884d54f7b92495be6766c1c5f0105761e72c0a3
keshavsoft <nknnkumar@yahoo.com>
2026-07-17 12:06:11 +0530

    to test

--- bin/v2/index.js
+++ bin/v2/index.js
@@ -1,37 +1,18 @@
-import path from "path";
+import getConfig from "./getConfig.js";
 
-import createHttpFile from "./createHttpFile.js";
-import getFile from "./getFile.js";
-import postFile from "./postFile.js";
-import deleteFile from "./deleteFile.js";
-import putFile from "./putFile.js";
-
-const startFunc = ({ toPath, toConfigPath, inTargetPath, showLog = true,
-    inPort, inMethod
+const startFunc = ({ toPath, configPath, inTargetPath, showLog = true,
+    inPort, inAction
 }) => {
     //toPath and inTargetPath are both needed as they will replace one another for end point generation
-    switch (inMethod) {
-        case "DELETE":
-            if (showLog) console.log("start rest generate for post...");
+    switch (inAction) {
+        case "Crud":
 
-            deleteFile({ toPath, toConfigPath, inTargetPath, inPort });
-            break;
-        case "PUT":
-            if (showLog) console.log("start rest generate for post...");
+            const columnsConfig = getConfig({ configPath });
 
-            putFile({ toPath, toConfigPath, inTargetPath, inPort });
             break;
 
         default:
-            if (toConfigPath) {
-                if (showLog) console.log("start rest generate for post...");
 
-                postFile({ toPath, toConfigPath, inTargetPath, inPort });
-            } else {
-                if (showLog) console.log("start rest generate for get...");
-
-                getFile({ toPath, inTargetPath, inPort });
-            };
             break;
     };
 
