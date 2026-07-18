422cbbe5d27f55f51a1e7488267f7c780af1ee3a
keshavsoft <nknnkumar@yahoo.com>
2026-07-18 10:27:20 +0530

    5 to test

--- test/v5/test.js
+++ test/v5/test.js
@@ -1,8 +1,13 @@
 import path from "path";
 import load from "../../index.js";
+import fs from "fs";
+
+const configPath = path.join(process.cwd(), "Config", "Schemas", "doctors.json");
+
+const doctorsJson = fs.readFileSync(configPath);
 
 load({
     toPath: path.join(process.cwd(), "doctors"),
-    configPath: path.join(process.cwd(), "Config", "Schemas", "doctors.json"),
+    sacredWisdom: JSON.parse(doctorsJson),
     inAction: "Crud"
 });
